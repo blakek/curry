41626eaa9444b6d0216d761da7ec983bcdd76537
Blake Knight <oss.ideas@gmail.com>
2020-06-14 18:51:12 -0500

    :zap: optimize build output; upgrade deps

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import { VariadicCurry } from './types';
 
 export * from './types';
 
+type GenericFn = (...args: any[]) => any;
+
 export function curry<T extends any[], R>(
   fn: (...args: T) => R,
   arity?: number
 ): VariadicCurry<T, R>;
 
-export function curry(fn: Function, arity: number = fn.length) {
-  return function curried(...args: any[]) {
+export function curry(fn: GenericFn, arity: number = fn.length) {
+  return function curried(...args: any[]): any {
     if (args.length >= arity) {
       return fn(...args.slice(0, arity));
     }
@@ -22,8 +24,8 @@ export function curryRight<T extends any[], R>(
   arity?: number
 ): VariadicCurry<T, R>;
 
-export function curryRight(fn: Function, arity: number = fn.length) {
-  return function curried(...args: any[]) {
+export function curryRight(fn: GenericFn, arity: number = fn.length) {
+  return function curried(...args: any[]): any {
     if (args.length >= arity) {
       return fn(...args.slice(0, arity).reverse());
     }
